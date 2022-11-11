a3cfa5361665f183d721442eeda2eb96fe32bb46
Arie0 <arielykarla3@gmail.com>
2022-11-11 20:18:00 -0300

    Project Created

---
+++ backend/src/main/java/com/ljinfo/ForInfo/ForInfoApplication.java
@@ -0,0 +1,13 @@
+package com.ljinfo.ForInfo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForInfoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForInfoApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/ljinfo/ForInfo/ForInfoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ljinfo.ForInfo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForInfoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
